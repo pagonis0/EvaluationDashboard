bb47e3a7c1ed2daa875c267d9b910aba0ac65a0e
Panos Pagonis <106801353+pagonis0@users.noreply.github.com>
2024-11-19 13:47:04 +0100

    Delete flaskdash directory

--- flaskdash/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import SelectField, BooleanField
-from wtforms.validators import DataRequired
-
-class DownloadForm(FlaskForm):
-    #name = SelectField('Name', validators=[DataRequired()], choices=[])
-    academic_year = SelectField('Studienjahr', validators=[DataRequired()], choices=[])
-    semester = SelectField('Semester', validators=[DataRequired()], choices=[])
-    course = SelectField('Kurs', validators=[DataRequired()], choices=[])   
